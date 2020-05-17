4850671a5702584c1221d233539b0ba8af1abcba
jbrick <jbrick91@gmail.com>
2020-05-17 13:12:17 -0400

    Hide engine configuration within the library

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -10,6 +10,26 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+    val djlEngine: String by project
+    when (djlEngine.toLowerCase()) {
+        "tensorflow" -> {
+            implementation("ai.djl.tensorflow:tensorflow-api:$djlVersion")
+            implementation("ai.djl.tensorflow:tensorflow-engine:$djlVersion")
+            implementation("ai.djl.tensorflow:tensorflow-model-zoo:$djlVersion")
+
+            implementation("ai.djl.tensorflow:tensorflow-native-auto:2.1.0-a-SNAPSHOT")
+        }
+        "mxnet" -> {
+            runtimeOnly("ai.djl.mxnet:mxnet-engine:$djlVersion")
+            runtimeOnly("ai.djl.mxnet:mxnet-native-auto:1.6.0")
+        }
+        "pytorch" -> {
+            runtimeOnly("ai.djl.pytorch:pytorch-engine:$djlVersion")
+            runtimeOnly("ai.djl.pytorch:pytorch-native-auto:1.5.0-SNAPSHOT")
+//            runtimeOnly("ai.djl.pytorch:pytorch-native-cpu:osx-x86_64:1.4.0")
+        }
+        else -> throw IllegalStateException("Unrecognized engine config: \"$djlEngine\"")
+    }
 
     tasks.test {
         useJUnitPlatform()
--- examples/mnist1d/build.gradle.kts
+++ examples/mnist1d/build.gradle.kts
@@ -20,31 +20,5 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
-
-    val djlVersion: String by project
-    val djlEngine: String by project
-    when (djlEngine.toLowerCase()) {
-        "tensorflow" -> {
-            implementation("ai.djl.tensorflow:tensorflow-api:$djlVersion")
-            implementation("ai.djl.tensorflow:tensorflow-engine:$djlVersion")
-            implementation("ai.djl.tensorflow:tensorflow-model-zoo:$djlVersion")
-
-            implementation("ai.djl.tensorflow:tensorflow-native-auto:2.1.0-a-SNAPSHOT")
-        }
-        "mxnet" -> {
-            runtimeOnly("ai.djl.mxnet:mxnet-engine:$djlVersion")
-            runtimeOnly("ai.djl.mxnet:mxnet-native-auto:1.6.0")
-        }
-        "pytorch" -> {
-            runtimeOnly("ai.djl.pytorch:pytorch-engine:$djlVersion")
-            runtimeOnly("ai.djl.pytorch:pytorch-native-auto:1.5.0-SNAPSHOT")
-//            runtimeOnly("ai.djl.pytorch:pytorch-native-cpu:osx-x86_64:1.4.0")
-        }
-        else -> throw IllegalStateException("Unrecognized engine config: \"$djlEngine\"")
-    }
 }
 
-
-val compileKotlin: KotlinCompile by tasks
-
-compileKotlin.kotlinOptions.jvmTarget = "1.8"
